97717e3018d3140d3591c2632adaabfb94397e21
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-08-07 18:25:20 +0900

    add colorTheme

--- src/Components/Footer.purs
+++ src/Components/Footer.purs
@@ -3,21 +3,30 @@ module Components.Footer where
 import Prelude
 
 import Contexts (Contexts)
-import Contexts.ColorMode (ColorScheme(..), ColorTarget(..), useColor)
+import Contexts.ColorMode (ColorMode(..), ColorScheme(..), ColorTarget(..), getColor, useColor, useColorMode)
+import Data.Tuple.Nested ((/\))
 import Hooks.UseClass (useClass)
-import Jelly (Component, Signal, ch, chSig, el, text, (:=))
+import Jelly (Component, Signal, ch, chSig, el, on, readSignal, text, writeAtom, (:=))
 
 footerComponent :: Component Contexts
 footerComponent = el "div" do
-  useClass $ pure "w-full h-32 py-3 px-6"
+  useClass $ pure "w-full h-32 py-4 px-12"
 
   useColor Reverse Text
   useColor Reverse Background
 
   useClass $ pure "text-opacity-80"
+  useClass $ pure "flex flex-row justify-between"
+
+  ch copyRight
+  ch colorThemes
+
+copyRight :: Component Contexts
+copyRight = el "div" do
+  useClass $ pure "text-opacity-80"
   useClass $ pure "flex flex-col gap-3"
 
-  ch $ el "p" $ ch $ text $ pure "©2022 YUKINET"
+  ch $ el "p" $ ch $ text $ pure "©2022-2022 YUKINET"
   ch $ el "p" do
     useClass $ pure "flex flex-row gap-2 items-baseline"
     ch $ linkText
@@ -43,3 +52,29 @@ linkText linkSig componentSig = el "a" do
   "rel" := pure "noopener noreferrer"
 
   chSig componentSig
+
+colorThemes :: Component Contexts
+colorThemes = el "div" do
+  useClass $ pure "flex flex-row gap-3"
+  ch $ colorThemeToggleButton $ pure Light
+  ch $ colorThemeToggleButton $ pure Dark
+  ch $ colorThemeToggleButton $ pure Summer
+  ch $ colorThemeToggleButton $ pure Winter
+  ch $ colorThemeToggleButton $ pure Hacker
+
+colorThemeToggleButton
+  :: Signal ColorMode
+  -> Component Contexts
+colorThemeToggleButton colorModeSig =
+  el "button" do
+    _ /\ colorThemeAtom <- useColorMode
+    on "click" \_ -> do
+      colorMode <- readSignal colorModeSig
+      writeAtom colorThemeAtom colorMode
+
+    useClass $ pure "w-6 h-6"
+    let
+      exampleColor = do
+        colorMode <- colorModeSig
+        pure $ getColor colorMode Highlight Background
+    useClass exampleColor
--- src/Contexts/ColorMode.purs
+++ src/Contexts/ColorMode.purs
@@ -12,7 +12,7 @@ import Web.HTML (window)
 import Web.HTML.Window (localStorage)
 import Web.Storage.Storage (getItem, setItem)
 
-data ColorMode = Light | Dark
+data ColorMode = Light | Dark | Summer | Winter | Hacker
 data ColorScheme = Primary | Highlight | Reverse | Disabled
 data ColorTarget = Text | Background
 
@@ -20,10 +20,16 @@ derive instance Eq ColorMode
 instance Show ColorMode where
   show Light = "light"
   show Dark = "dark"
+  show Summer = "summer"
+  show Winter = "winter"
+  show Hacker = "hacker"
 
 readColorMode :: String -> Maybe ColorMode
 readColorMode "light" = Just Light
 readColorMode "dark" = Just Dark
+readColorMode "summer" = Just Summer
+readColorMode "winter" = Just Winter
+readColorMode "hacker" = Just Hacker
 readColorMode _ = Nothing
 
 defaultColorMode :: ColorMode
@@ -62,30 +68,69 @@ getColor :: ColorMode -> ColorScheme -> ColorTarget -> String
 getColor = case _ of
   Light -> case _ of
     Primary -> case _ of
-      Text -> "text-slate-900"
+      Text -> "text-neutral-900"
       Background -> "bg-white"
     Highlight -> case _ of
       Text -> "text-rose-600"
       Background -> "bg-rose-600"
     Reverse -> case _ of
       Text -> "text-white"
-      Background -> "bg-slate-900"
+      Background -> "bg-neutral-800"
     Disabled -> case _ of
-      Text -> "text-slate-600"
+      Text -> "text-neutral-600"
       Background -> "bg-white bg-opacity-80"
   Dark -> case _ of
     Primary -> case _ of
       Text -> "text-white"
       Background -> "bg-slate-900"
     Highlight -> case _ of
-      Text -> "text-rose-600"
-      Background -> "bg-rose-600"
+      Text -> "text-fuchsia-500"
+      Background -> "bg-fuchsia-500"
     Reverse -> case _ of
       Text -> "text-slate-900"
       Background -> "bg-white"
     Disabled -> case _ of
       Text -> "text-slate-300"
       Background -> "bg-slate-900 bg-opacity-80"
+  Summer -> case _ of
+    Primary -> case _ of
+      Text -> "text-slate-800"
+      Background -> "bg-white"
+    Highlight -> case _ of
+      Text -> "text-sky-600"
+      Background -> "bg-sky-600"
+    Reverse -> case _ of
+      Text -> "text-white"
+      Background -> "bg-slate-700"
+    Disabled -> case _ of
+      Text -> "text-slate-500"
+      Background -> "bg-white bg-opacity-80"
+  Winter -> case _ of
+    Primary -> case _ of
+      Text -> "text-white"
+      Background -> "bg-stone-900"
+    Highlight -> case _ of
+      Text -> "text-stone-400"
+      Background -> "bg-stone-400"
+    Reverse -> case _ of
+      Text -> "text-stone-900"
+      Background -> "bg-stone-100"
+    Disabled -> case _ of
+      Text -> "text-stone-300"
+      Background -> "bg-stone-900 bg-opacity-80"
+  Hacker -> case _ of
+    Primary -> case _ of
+      Text -> "text-white"
+      Background -> "bg-black"
+    Highlight -> case _ of
+      Text -> "text-green-500"
+      Background -> "bg-green-500"
+    Reverse -> case _ of
+      Text -> "text-black"
+      Background -> "bg-white"
+    Disabled -> case _ of
+      Text -> "text-slate-300"
+      Background -> "bg-black bg-opacity-80"
 
 useColorSig
   :: forall r
