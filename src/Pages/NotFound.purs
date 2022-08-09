6365eb7fe0e474d4adb28b0ae97c3efef0b88762
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-08-09 14:47:32 +0900

    add glitch

--- src/Pages/NotFound.purs
+++ src/Pages/NotFound.purs
@@ -2,10 +2,10 @@ module Pages.NotFound where
 
 import Prelude
 
-import Components.PageTitle (pageTitleComponent)
 import Contexts (Contexts)
+import Data.Array (replicate)
 import Hooks.UseClass (useClass)
-import Jelly (Component, Signal, ch, el, text, (:=))
+import Jelly (Component, Signal, ch, chs, el, text, (:=))
 
 notFoundPageComponent :: Signal String -> Component Contexts
 notFoundPageComponent pathSig = el "div" do
@@ -14,25 +14,39 @@ notFoundPageComponent pathSig = el "div" do
   useClass $ pure "flex flex-col items-center p-12 gap-12"
 
   ch $ el "div" do
-    useClass $ pure "flex flex-col p-20 gap-6"
-    useClass $ pure "bg-[#0000ff] text-white rounded-md"
+    useClass $ pure "flex flex-col p-20 gap-6 relative overflow-hidden"
+    useClass $ pure
+      "bg-[#0000ff] text-white text-opacity-80 rounded-md bg-gradient-radial from-[#0000ff] to-[#0000cc]"
 
     ch $ el "div" do
 
-      useClass $ pure "text-3xl md:text-5xl mb-10"
+      useClass $ pure "text-3xl md:text-5xl mb-10 animate-glitch-text"
       ch $ text $ pure "˚‧º·(˚ ˃̣̣̥᷄⌓˂̣̣̥᷅ )‧º·˚ "
 
     ch $ el "div" do
-      useClass $ pure "text-lg"
+      useClass $ pure "text-lg font-mono animate-glitch-text"
       ch $ text $
         ( \p -> "Seems that you came to a page that doesn't exist. (#" <> p <>
             ")"
         ) <$>
           pathSig
     ch $ el "div" do
-      useClass $ pure "flex flex-row gap-3 items-start"
+      useClass $ pure
+        "flex font-mono flex-row gap-3 items-start"
       ch $ el "img" do
+        useClass $ pure "animate-glitch opacity-80"
         "src" := pure "./img/QR.png"
         useClass $ pure "w-24 h-24 mt-2"
       ch $ el "div" do
+        useClass $ pure "animate-glitch-text"
         ch $ text $ pure "code: 404"
+
+    ch $ el "div" do
+      useClass $ pure "absolute top-0 left-0 w-full h-full z-10 "
+      useClass $ pure "flex flex-col"
+      chs $ replicate 100 $ el "div" do
+        useClass $ pure "flex-1 overflow-hidden"
+
+        ch $ el "div" do
+          useClass $ pure
+            "h-[1px] bg-[#000000] opacity-20"
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,75 @@ module.exports = {
   content: ["./*.html", "./src/**/*.purs"],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
       fontFamily: {
         Rubik: ["Rubik", "sans-serif"],
         ZenKaku: ["Zen Kaku Gothic New", "sans-serif"],
         Bungee: ["Bungee", "cursive"],
-        SourceCodePro: ['Source Code Pro', 'monospace'],
+        SourceCodePro: ["Source Code Pro", "monospace"],
         default: ["Rubik", "Zen Kaku Gothic New", "sans-serif"],
+      },
+      animation: {
+        "glitch-text": "glitch-text 3s linear infinite",
+        "glitch": "glitch 3s linear infinite",
+      },
+      keyframes: {
+        "glitch-text": {
+          "0%, 9%, 11%, 39%, 41%, 69%, 71%, 79%, 81%, 100%": {
+            "text-shadow":
+              "0px 0px 6px black, 0px 0px 3px #00A75B, 0px 0px 3px #96007A",
+            transform: "translate(0px)",
+          },
+          "10%": {
+            "text-shadow":
+              "0px 0px 6px black, 5px 0px 3px #00A75B, -3px 0px 3px #96007A",
+            transform: "translate(-1px)",
+          },
+          "40%": {
+            "text-shadow":
+              "0px 0px 6px black, -5px 0px 3px #96007A, 3px 0px 3px #00A75B",
+            transform: "translate(2px)",
+          },
+          "70%": {
+            "text-shadow":
+              "0px 0px 6px black, 5px 0px 3px #00A75B, -3px 0px 3px #96007A",
+            transform: "translate(-3px)",
+          },
+          "80%": {
+            "text-shadow":
+              "0px 0px 6px black, -5px 0px 3px #00A75B, 3px 0px 3px #96007A",
+            transform: "translate(2px)",
+          },
+        },
+        "glitch": {
+          "0%, 9%, 11%, 39%, 41%, 69%, 71%, 79%, 81%, 100%": {
+            "box-shadow":
+              "0px 0px 6px black, 0px 0px 3px #00A75B, 0px 0px 3px #96007A",
+            transform: "translate(0px)",
+          },
+          "10%": {
+            "box-shadow":
+              "0px 0px 6px black, 5px 0px 3px #00A75B, -3px 0px 3px #96007A",
+            transform: "translate(-1px)",
+          },
+          "40%": {
+            "box-shadow":
+              "0px 0px 6px black, -5px 0px 3px #96007A, 3px 0px 3px #00A75B",
+            transform: "translate(2px)",
+          },
+          "70%": {
+            "box-shadow":
+              "0px 0px 6px black, 5px 0px 3px #00A75B, -3px 0px 3px #96007A",
+            transform: "translate(-3px)",
+          },
+          "80%": {
+            "box-shadow":
+              "0px 0px 6px black, -5px 0px 3px #00A75B, 3px 0px 3px #96007A",
+            transform: "translate(2px)",
+          },
+        },
       },
     },
   },
