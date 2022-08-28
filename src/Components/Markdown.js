cd60c35709517ed0e06b6b2412d702827132cf82
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-08-28 16:48:08 +0900

    fix hljs error

--- src/Components/Markdown.js
+++ src/Components/Markdown.js
@@ -1,5 +1,5 @@
 import { marked } from "marked";
-import hljs from "highlight.js/libs/highlight";
+import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 import css from "highlight.js/lib/languages/css";
 
